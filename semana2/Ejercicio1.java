4101e9a5ae3cf8410752e7325166e36d273ff62f
123adolfoelzea-cpu <123adolfoelzea@gmail.com>
2026-02-10 16:30:57 +0000

    Se agrego un numero al arreglo

--- semana2/Ejercicio1.java
+++ semana2/Ejercicio1.java
@@ -4,10 +4,10 @@ public class Ejercicio1 {
     
     public static void main(String[] args) {
         //Forma1 -> Definiendo dierctamente Los valores del arreglo
-        int[] a ={4, 8, 9, 6, 1, 2};
+        int[] a ={4, 8, 9, 6, 1, 2, 10};
         //iterar los elementos del arreglo
         for(int i=0; i< a.length; i++){
-            System.out.println("a ["+ i +"]="+ a [i]);
+            System.out.println("a["+ i +"]="+ a[i]);
         }
     }
 }
